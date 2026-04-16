d055e098398a213064bbbf503fbd550b7e419c48
Theo Cavaciuti <theocavaciuti@gmail.com>
2026-04-16 19:19:53 +0100

    commit

--- assets/js/env.js
+++ assets/js/env.js
@@ -3,5 +3,5 @@ window.__POLYPLACES_ENV__ = {
   "POLYPLACES_SITE_URL": "polyplaces.co.uk",
   "POLYPLACES_NOMINATIM_URL": "https://nominatim.openstreetmap.org/search",
   "POLYPLACES_SEARCH_COUNTRY_CODES": "gb",
-  "POLYPLACES_SEARCH_VIEWBOX": "-8.7,60.9,1.9,49.8"
+  "POLYPLACES_SEARCH_VIEWBOX": "-8.7,60.9,1.9,49.8" 
 };
